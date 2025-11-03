93352b1660395f15b555df4a9cff26d1a62190ff
CyanCool <juliannagaertner@gmail.com>
2025-11-03 16:39:40 -0700

    UserServiceTests written, not yet tested

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,10 +1,10 @@
 package service;
 
 import chess.ChessGame;
+import model.*;
 import org.junit.jupiter.api.*;
 import passoff.model.TestAuthResult;
 import service.UserService;
-import model.RegisterRequest;
 
 import java.net.HttpURLConnection;
 import java.util.*;
@@ -34,9 +34,54 @@ public class UserServiceTests
         //check the database to make sure it doesn't add the user twice, maybe
     }
 
-    public void tryBadRequest()
+    @Test
+    @Order(2)
+    @DisplayName("Bad Password Request")
+    public void tryBadPassword()
     {
         RegisterRequest myRequest = new RegisterRequest("Rose", null, "rose@gmail.com");
         userService.register(myRequest);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Bad Email Request")
+    public void tryBadEmail()
+    {
+        RegisterRequest myRequest = new RegisterRequest("Rose", "password", null);
+        userService.register(myRequest);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Bad Username Request")
+    public void tryBadUsername()
+    {
+        RegisterRequest myRequest = new RegisterRequest(null, "password", "rose@gmail.com");
+        userService.register(myRequest);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Successful Registration")
+    public void registerSuccess()
+    {
+        RegisterRequest myRequest = new RegisterRequest("Bartholemue", "Imanerd", "bartholemue@gmail.com");
+        RegisterResponse myResponse = userService.register(myRequest);
+
+        //make sure the username of the request matches the response
+        if(!myRequest.username().equals(myResponse.username()))
+        {
+            System.out.println("Registration Request did not have the same username as registration response");
+        }
+        //make sure the authtoken in the response isn't null
+        else if(myResponse.authtoken() == null)
+        {
+            System.out.println("The authToken is null");
+        }
+        else
+        {
+            System.out.println("Registration was successful");
+        }
     }
 }
