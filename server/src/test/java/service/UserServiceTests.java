101f630e155316c7ca0b8b03801b86231eead3ba
CyanCool <juliannagaertner@gmail.com>
2025-11-03 17:29:26 -0700

    commit test

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -5,16 +5,12 @@ import model.*;
 import org.junit.jupiter.api.*;
 import passoff.model.TestAuthResult;
 import service.UserService;
-
 import java.net.HttpURLConnection;
 import java.util.*;
+import exception.*;
 
 public class UserServiceTests
 {
-
-    //test register
-        //try making a login with a username that is already taken
-        //try logging in with a unique username
     private UserService userService;
     public UserServiceTests()
     {
@@ -27,11 +23,7 @@ public class UserServiceTests
     public void tryUserTaken()
     {
         RegisterRequest myRequest = new RegisterRequest("Steven", "password", "steven@gmail.com");
-        userService.register(myRequest);
-        userService.register(myRequest);
-
-        //check to see if it returns the exception
-        //check the database to make sure it doesn't add the user twice, maybe
+        Assertions.assertDoesNotThrow(() -> {userService.register(myRequest); }); //check to see if it returns the exception
     }
 
     @Test
@@ -40,7 +32,7 @@ public class UserServiceTests
     public void tryBadPassword()
     {
         RegisterRequest myRequest = new RegisterRequest("Rose", null, "rose@gmail.com");
-        userService.register(myRequest);
+        Assertions.assertDoesNotThrow(() -> {userService.register(myRequest); });
     }
 
     @Test
@@ -49,7 +41,7 @@ public class UserServiceTests
     public void tryBadEmail()
     {
         RegisterRequest myRequest = new RegisterRequest("Rose", "password", null);
-        userService.register(myRequest);
+        Assertions.assertDoesNotThrow(() -> {userService.register(myRequest); });
     }
 
     @Test
@@ -58,7 +50,7 @@ public class UserServiceTests
     public void tryBadUsername()
     {
         RegisterRequest myRequest = new RegisterRequest(null, "password", "rose@gmail.com");
-        userService.register(myRequest);
+        Assertions.assertDoesNotThrow(() -> {userService.register(myRequest); });
     }
 
     @Test
@@ -70,18 +62,8 @@ public class UserServiceTests
         RegisterResponse myResponse = userService.register(myRequest);
 
         //make sure the username of the request matches the response
-        if(!myRequest.username().equals(myResponse.username()))
-        {
-            System.out.println("Registration Request did not have the same username as registration response");
-        }
+        Assertions.assertEquals(myRequest.username(), myResponse.username());
         //make sure the authtoken in the response isn't null
-        else if(myResponse.authtoken() == null)
-        {
-            System.out.println("The authToken is null");
-        }
-        else
-        {
-            System.out.println("Registration was successful");
-        }
+        Assertions.assertNotNull(myResponse.authtoken());
     }
 }
