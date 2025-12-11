c7bad44866fd72f0b3163de768cb6c9e3b3173ec
CyanCool <juliannagaertner@gmail.com>
2025-12-10 18:09:36 -0700

    Code quality

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -19,16 +19,6 @@ public class ResponseException extends Exception {
         this.code = code;
     }
 
-    public String toJson() {
-        return new Gson().toJson(Map.of("message", getMessage(), "status", code));
-    }
-
-    public static ResponseException fromJson(String json) {
-        var map = new Gson().fromJson(json, HashMap.class);
-        String message = map.get("message").toString();
-        return new ResponseException(Code.ServerError, message);
-    }
-
     public Code code() {
         return code;
     }
